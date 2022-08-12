91da475b4c4e92b57d3d3fae3bbccbead7f90899
unknown <subhankarmondal1995@yahoo.com>
2022-08-12 17:43:53 +0530

    simple Flask app

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask 
+
+
+app = Flask(__name__)
+
+
+@app.route("/",methods=['GET','POST'])
+def index():
+    return "To start machine learning Project"
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
